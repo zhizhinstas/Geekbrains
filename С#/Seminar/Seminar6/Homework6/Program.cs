3f96bc9a93e83a938076720cbb897f7d218f3811
ZhizhinStas <my@zhizhinstas.ru>
2024-02-25 01:43:10 +0300

    createHW6

---
+++ С#/Seminar/Seminar6/Homework6/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 1: Задайте двумерный массив символов (тип char [,]). Создать строку из символов этого массива.
+Console.Clear();
+
+// char[,] array = new char[,]
+// {
+//     {'a', 'b', 'c'},
+//     {'d', 'e', 'f'},
+//     {'g', 'h', 'i'}
+// };
+
+// string result = string.Empty;
+
+// for (int i = 0; i < array.GetLength(0); i++)
+// {
+//     for (int j = 0; j < array.GetLength(1); j++)
+//     {
+//         result += array[i, j];
+//     }
+// }
+
+// Console.WriteLine(result);
+
+// Задача 2: Задайте строку, содержащую латинские буквы в обоих регистрах. 
+// Сформируйте строку, в которой все заглавные буквы заменены на строчные.
+
+// string inputString = "HeLLoWorld";
+// string convertedString = inputString.ToLower();
+// Console.Write (convertedString);
+
+// Задача 3: Задайте произвольную строку. Выясните, является ли она палиндромом.
+
